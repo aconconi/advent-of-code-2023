8a4b390c65321319617e6f14cd06aa700bce4df1
aconconi <aconconi@users.noreply.github.com>
2023-12-25 23:04:00 +0100

    day 19 solution

--- aoc2023_day19.py
+++ aoc2023_day19.py
@@ -4,114 +4,146 @@
 """
 
 
-import pytest
-import re
-
-from dataclasses import dataclass
-from operator import lt, gt
 import re
 from math import prod
 
+import pytest
 
-@dataclass
+
 class Part:
-    x: int
-    m: int
-    a: int
-    s: int
+    """Class representing a Part with x, m, a, s attributes."""
+
+    pattern = re.compile(r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}")
+
+    def __init__(self, x, m, a, s):
+        self.x = x
+        self.m = m
+        self.a = a
+        self.s = s
 
     @classmethod
     def from_string(cls, string: str) -> "Part":
-        pattern = re.compile(r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}")
-        match = pattern.match(string)
+        match = cls.pattern.match(string)
         if not match:
             raise ValueError("Invalid string format for Part")
-        
         x, m, a, s = map(int, match.groups())
         return cls(x, m, a, s)
 
-    def apply_workflow(self, workflows, name):
-        if name in {"A", "R"}:
-            return name == "A"
-
-        conditions, default = workflows[name]
-        for condition in conditions:
-            field, op, comp_value, result = re.match(
-                r"(\w+)([<>]=?)(\d+):(\w+)", condition
-            ).groups()
-            comp_func = lt if op == "<" else gt
-            if comp_func(getattr(self, field), int(comp_value)):
-                return self.apply_workflow(workflows, result)
-        return self.apply_workflow(workflows, default)
+    def __getitem__(self, field):
+        return getattr(self, field)
 
     def total_rating(self):
-        return self.x + self.m + self.a + self.s
+        return sum(getattr(self, attr) for attr in ("x", "m", "a", "s"))
+
+    def __repr__(self):
+        return f"(x:{self.x}, m:{self.m}, a:{self.a}, s:{self.s})"
+
+    def apply_workflow(self, workflows, initial_state):
+        current_workflow = workflows[initial_state]
+
+        while current_workflow:
+            for rule in current_workflow:
+                match outcome := rule.evaluate_part(self):
+                    case "A":
+                        return True
+                    case "R":
+                        return False
+                    case None:
+                        continue
+                    case _:
+                        current_workflow = workflows[outcome]
+                        break
 
 
-def decode_workflow(line):
-    matched = re.match(r"(\w+){(.*)}", line)
-    if not matched:
-        return None
-    name, conditions = matched.groups()
-    conditions = conditions.split(",")
-    return name, (conditions[:-1], conditions[-1])
+class Rule:
+    pattern = re.compile(r"([a-z]+)(.)(\d+):(.+)")
+
+    def __init__(self, condition, target):
+        self.condition, self.target = condition, target
+
+    @classmethod
+    def from_string(cls, rule_s: str):
+        match_object = cls.pattern.search(rule_s)
+        if match_object:
+            key, op, val, target = match_object.groups()
+            condition = (key, op, int(val))
+        else:
+            condition, target = None, rule_s
+        return cls(condition, target)
+
+    def inverted_condition(self):
+        key, op, val = self.condition
+        new_op, new_val = (">", val - 1) if op == "<" else ("<", val + 1)
+        return (key, new_op, new_val)
+
+    def evaluate_part(self, part):
+        if self.condition is None:
+            return self.target
+        key, op, val = self.condition
+        return (
+            self.target
+            if (op == "<" and part[key] < val) or (op == ">" and part[key] > val)
+            else None
+        )
+
+    def __repr__(self):
+        return f"({self.condition}, {self.target})"
 
 
 def parse_input(file_name):
     with open(file_name, "r", encoding="ascii") as data_file:
-        workflows, ratings = data_file.read().split("\n\n")
-        return workflows.splitlines(), ratings.splitlines()
+        wf_data, parts_data = data_file.read().split("\n\n")
+    workflows = {
+        name: [Rule.from_string(rule_s) for rule_s in rules_s.split(",")]
+        for name, rules_s in (
+            line.rstrip("}").split("{") for line in wf_data.splitlines()
+        )
+    }
+    parts = [Part.from_string(line) for line in parts_data.splitlines()]
+    return workflows, parts
+
+
+def add_constraint(constraints, condition):
+    # credit: https://github.com/davearussell/advent2023
+    key, op, val = condition
+    low, high = constraints.get(key, (1, 4000))
+    if op == ">":
+        if val >= high:
+            return None
+        low = val + 1
+    else:
+        if val <= low:
+            return None
+        high = val - 1
+    return dict(constraints, **{key: (low, high)})
+
+
+def trace_paths(workflows, state):
+    name, constraints = state
+    for rule in workflows[name]:
+        if rule.condition is None:
+            cons_true = constraints
+        else:
+            cons_true = add_constraint(constraints, rule.condition)
+            constraints = add_constraint(constraints, rule.inverted_condition())
+
+        if cons_true is not None:
+            if rule.target == "A":
+                yield cons_true
+            elif rule.target != "R":
+                yield from trace_paths(workflows, (rule.target, cons_true))
 
 
 def day19_part1(data):
-    wf_data, parts_data = data
-    workflows = dict(decode_workflow(line) for line in wf_data)
-    parts = [Part.from_string(line) for line in parts_data]
-    return sum(
-        part.total_rating() for part in parts if part.apply_workflow(workflows, "in")
-    )
+    workflows, parts = data
+    return sum(p.total_rating() for p in parts if p.apply_workflow(workflows, "in"))
 
-
-class PartWithRange:
-    def __init__(self):
-        self.intervals = {"x": (1, 4000), "m": (1, 4000), "a": (1, 4000), "s": (1, 4000)}
-
-    def split_interval(self, condition):
-        field, op, comp_value, result = re.match(
-            r"(\w+)([<>]=?)(\d+):(\w+)", condition
-        ).groups()
-        
-
-        low, high = self.intervals[field] 
-        cond_low =  (lt if op == "<" else gt)(low, int(comp_value))
-        cond_high =  (lt if op == "<" else gt)(high, int(comp_value))
-
-        if cond_low and cond_high:
-            matching_interval = (low, high)
-            non_matching_interval = None
-        elif not cond_low and not cond_high:
-            matching_interval = None
-            non_matching_interval = (low, high)
-        else:
-            if cond_low:
-                matching_interval = (low, high - 1)
-                non_matching_interval = (high, high)
-            else:
-                matching_interval = (low + 1, high)
-                non_matching_interval = (low, low)
-        return matching_interval, non_matching_interval
-
-
-    def total_combinations(self):
-        return prod(high - low for low, high in self.intervals.values())
-    
 
 def day19_part2(data):
-    start = ({"x": (1, 4000), "m": (1, 4000), "a": (1, 4000), "s": (1, 4000)}, "in")
-
-    todo = [start]
-
-
+    workflows, _ = data
+    initial_state = ("in", {field: (1, 4000) for field in "xmas"})
+    paths = trace_paths(workflows, initial_state)
+    return sum(prod(hi - lo + 1 for lo, hi in path.values()) for path in paths)
 
 
 @pytest.fixture(autouse=True, name="test_data")
@@ -123,10 +155,9 @@ def test_day19_part1(test_data):
     assert day19_part1(test_data) == 19114
 
 
-"""
 def test_day19_part2(test_data):
-    assert day19_part2(test_data)
-"""
+    assert day19_part2(test_data) == 167409079868000
+
 
 if __name__ == "__main__":
     input_data = parse_input("data/day19.txt")
@@ -135,4 +166,4 @@ if __name__ == "__main__":
     print(day19_part1(input_data))  # Correct answer is 353046
 
     print("Day 19 Part 2:")
-    print(day19_part2(input_data))  # Correct answer is
+    print(day19_part2(input_data))  # Correct answer is 125355665599537
